967516b055966f9fa429c4e4b3054b86f6000621
larson <you@example.com>
2017-07-21 09:43:12 +0200

    modification de la partie connexion

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -1,3 +1,36 @@
+<?php 
+    /* code pour la connexion */
+    /* vérifi si le formulaire est valide*/
+    if(isset($_POST['email'], $_POST['mdp']))
+    {
+        $email  = $_POST['email'];
+        $mdp    = $_POST['mdp'];
+
+        /* vérifie si l'utilisateur existe */
+        $user_exist = $pdo->prepare("SELECT * FROM membre WHERE email = ?");
+        $user_exist->execute([$email]);
+
+        /* si l'utilisateur existe et si le mot de passe est correct on le connecte */
+        $utilisateur = $user_exist->fetch(PDO::FETCH_ASSOC);
+        if($utilisateur && password_verify($mdp, $utilisateur['mdp']))
+        {
+            utilisateursession($utilisateur);
+            $message = '<div class="alert alert-success"><strong>Félicitation!</strong> Vout êtes connecté</div>';
+        }else
+        {
+            $message = '<div class="alert alert-danger">l\'Email ou le Mot de passe sont incorrecte!<br> Merci de réessayer.</div>';
+        }
+    }
+
+        /* gestion de la deconnexion */
+        if(isset($_GET['action']) && $_GET['action'] == 'deconnexion')
+        {
+            unset($_SESSION['utilisateur']);
+            $message = '<div class="alert alert-success">Vous avez été deconnecté.</div>';
+            header("Refresh: 3; URL=index.php");    
+        }
+    
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
